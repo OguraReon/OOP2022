351a9ffb73330452896d5108d6fb9c523516dd0d
伶音 小倉 <33127@ojs.ac.jp>
2022-04-26 15:32:53 +0900

    P62まで　売り上げデータを読み込み、Saleオブジェクトのリストを返す

---
+++ Chapter02/salesCalculater/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salesCalculater {
+    class Program {
+        static void Main(string[] args) {
+            ReadSales("sales.csv");
+        }
+
+        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
+        static List<Sale> ReadSales(string  filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach  (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
+    }
+}
