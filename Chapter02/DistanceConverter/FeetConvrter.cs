75c160474b0c5dcf7b6ae711916a47a676c1b39a
伶音 小倉 <33127@ojs.ac.jp>
2022-04-26 14:04:07 +0900

    FeetConverterクラス

--- Chapter02/DistanceConverter/FeetConvrter.cs
+++ Chapter02/DistanceConverter/FeetConvrter.cs
@@ -6,15 +6,16 @@ using System.Threading.Tasks;
 
 namespace DistanceConverter {
    public static class FeetConverter {
+        private const double ratio = 0.3048; //定数を定義
+        //public const readonly double ratio = 0.03048;外部からもアクセスさせたい場合
+
         //メートルからフィートを求める(静的メソッド)
         public static double FromMeter(int meter) {
-         
-        return meter / 0.3048;
+        return meter / ratio;
     }
         //メートルからフィートを求める(静的メソッド)
         public static double ToMeter(int feet) {
-            return feet / 0.3048;
+            return feet / ratio;
         }
-
     }
 }
