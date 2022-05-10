f62d19b25869037916c087ba1fac3bf3842257c9
伶音 小倉 <33127@ojs.ac.jp>
2022-05-10 13:47:48 +0900

    演習問題2.1の２

--- Chapter02/Exercise/Song.cs
+++ Chapter02/Exercise/Song.cs
@@ -5,9 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercise {
-    class Song {
-        string Title; //歌のタイトル
-        string ArtistName; //アーティスト名
-        int Length;//演奏時間、単位は秒
+    public class Song {
+        //メンバ変数
+        private string Title; //歌のタイトル
+        private string ArtistName; //アーティスト名
+        private int Length;//演奏時間、単位は秒
+
+        //コンストラクタ
+        public Song(string Title, string ArtistName, int Length) {
+            this.Title = Title;
+            this.ArtistName = ArtistName;
+            this.Length = Length;
+
+        }
     }
+    
 }
