76c15e7b572f87ed9258ea4adfd4ec1bbd288717
伶音 小倉 <33127@ojs.ac.jp>
2022-05-12 15:54:16 +0900

    演習問題２．３

---
+++ Exercise02/Exercise03/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salesCalculater {
+    class Program {
+
+        static void Main(string[] args) {
+            int select = 0;
+            var sales = new SalesCounter("sales.csv");
+            IDictionary<string, int> amountPerStore = null;
+
+            while (true) {
+                Console.WriteLine("売上高表示選択");
+                Console.WriteLine("1.店舗別");
+                Console.WriteLine("2.商品カテゴリー");
+                Console.Write(">");
+
+                select = int.Parse(Console.ReadLine());
+                
+                switch (select) {
+                    case 1:
+                        amountPerStore = sales.GetPerStoreSales();
+                        break;
+
+                    case 2:
+                        amountPerStore = sales.GetPerCategorySales();
+                        break;
+                    case 999:
+
+                        return;
+
+                }
+
+                foreach (var obj in amountPerStore) {
+                    Console.WriteLine("{0} {1}", obj.Key, obj.Value);
+                }
+
+            }
+
+            
+
+
+
+        }
+
+    }
+}
