9b2b43ae4e92e24202d091aeb1b1da65a62e693b
伶音 小倉 <33127@ojs.ac.jp>
2022-05-13 15:16:52 +0900

    ラムダ演算子

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Section01 {
+    class Program {
+       
+
+        static void Main(string[] args) {
+            var list = new List<string> {
+               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            /*var exists = list.Exists(s => s[0] == 'A');
+            Console.WriteLine(exists);*/
+
+            var names = list.FindAll(s => s.Length <= 5);
+            names.ForEach(s => Console.WriteLine(s));
+            
+            
+            //foreach (var s in names) {
+            //    Console.WriteLine(s);
+           // }
+            
+        }
+
+    }
+}
