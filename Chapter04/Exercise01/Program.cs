6330989687c8af7b6983f69bbb83d40408027bfd
伶音 小倉 <33127@ojs.ac.jp>
2022-05-20 15:55:11 +0900

    オーバーライド

---
+++ Chapter04/Exercise01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+
+            var ym = new YearMonth(2022, 12);
+
+            var check = ym.Is21Century;
+            var test = ym.AddOneMonth();
+
+            Console.WriteLine(ym.ToString());
+          
+        }
+    }
+}
---
+++ Chapter04/Exercise01/YearMonth.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    public class YearMonth {
+
+        //4.1.1
+        public int Year { get; private set; }//読み取り専用
+        public int Month { get; private set; }//読み取り専用
+
+        public YearMonth(int year, int month) {
+            Year = year;
+            Month = month;
+        }
+        //4.1.2
+        public bool Is21Century {
+            get {
+                return 2001 <=Year && Year <= 2100;
+            }
+        }
+        public YearMonth AddOneMonth() {           
+            if(12 == Month) {
+               return new YearMonth(this.Year++, 1);
+
+            } else {
+              return  new YearMonth(this.Year, this.Month++);
+
+            }
+        }
+
+        //4.1.4
+        public override string ToString() {
+            //return Year + "年" + Month + "月";
+            return $"{Year}年{Month}月";
+        }
+    }
+}
