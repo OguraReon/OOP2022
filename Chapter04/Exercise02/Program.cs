5e3e5da93270a4f81ae767c2299cae6809a4a039
伶音 小倉 <33127@ojs.ac.jp>
2022-05-24 13:28:28 +0900

    問題４．２の作成準備 問題４．１のクラスライブラリ化

--- Chapter04/Exercise02/Program.cs
+++ Chapter04/Exercise02/Program.cs
@@ -1,20 +1,20 @@
-﻿using System;
+﻿using Exercise01;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise01 {
+namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
-
-            var ym = new YearMonth(2022, 12);
+           /* var ym = new YearMonth(2022, 12);
 
             var check = ym.Is21Century;
             var test = ym.AddOneMonth();
 
-            Console.WriteLine(ym.ToString());
-          
+            Console.WriteLine(ym);
+           */
         }
     }
 }
