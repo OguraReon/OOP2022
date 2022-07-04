3aedc90b9c1996ed052c69ba0cf208b705eace64
伶音 小倉 <33127@ojs.ac.jp>
2022-07-04 11:57:49 +0900

    演習問題１１の２の途中

---
+++ Chapter11/Exercise11/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Exercise11 {
+    class Program {
+        static void Main(string[] args) {
+            var file = "sample.xml";
+            Exercise1_1(file);
+            Console.WriteLine();
+            Exercise1_2(file);
+            Console.WriteLine();
+            Exercise1_3(file);
+            Console.WriteLine();
+
+            var newfile = "sports.xml";
+            Exercise1_4(file, newfile);
+        }
+
+        private static void Exercise1_1(string file) {
+            var xdoc = XDocument.Load(file);
+            var samples = xdoc.Root.Elements().Select(x => new {
+                Name = x.Element("name").Value,
+                TeamMember = x.Element("teammembers").Value
+            });
+            foreach (var sample in samples) {
+                Console.WriteLine("{0}部 {1}人", sample.Name, sample.TeamMember);
+            }
+        }
+
+        private static void Exercise1_2(string file) {
+            var xdoc = XDocument.Load(file);
+            var samples = xdoc.Root.Elements().Select(x => new {
+                Name = x.Element("name").Attribute("kanji").Value,
+                PlayDate = x.Element("firstplayed").Value
+            });
+            foreach (var sample in samples.OrderBy(x => x)) {
+                Console.WriteLine("{0}部 {1}年", sample.Name, sample.PlayDate);
+            }
+
+        }
+
+        private static void Exercise1_3(string file) {
+            
+        }
+
+        private static void Exercise1_4(string file, string newfile) {
+           
+        }
+    }
+}
