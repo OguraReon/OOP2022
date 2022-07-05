4e8c8ad0f96b11b0d8aad693c3c01bdd3abbe2c5
伶音 小倉 <33127@ojs.ac.jp>
2022-07-05 15:56:49 +0900

    シリアル化

--- Chapter11/Exercise11/Program.cs
+++ Chapter11/Exercise11/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 
@@ -15,6 +16,9 @@ namespace Exercise11 {
 
             var newfile = "sports.xml";
             Exercise1_4(file, newfile);
+
+            var test = File.ReadAllText(newfile);
+            Console.WriteLine(test);
         }
 
         private static void Exercise1_1(string file) {
@@ -30,21 +34,35 @@ namespace Exercise11 {
 
         private static void Exercise1_2(string file) {
             var xdoc = XDocument.Load(file);
-            var samples = xdoc.Root.Elements().Select(x => new {
-                Name = x.Element("name").Attribute("kanji").Value,
-                PlayDate = x.Element("firstplayed").Value
-            });
-            foreach (var sample in samples.OrderBy(x => x)) {
+            var samples = xdoc.Root.Elements()
+                            .Select(x => new {
+                 Name = x.Element("name").Attribute("kanji").Value,
+                 PlayDate = x.Element("firstplayed").Value
+             });
+            foreach (var sample in samples.OrderBy(x => x.PlayDate)) {
                 Console.WriteLine("{0}部 {1}年", sample.Name, sample.PlayDate);
             }
 
         }
 
         private static void Exercise1_3(string file) {
-            
+            var xdoc = XDocument.Load(file);
+            var samples = xdoc.Root.Elements().Select(x => new {
+                Name = x.Element("name").Value,
+                TeamMember = x.Element("teammembers").Value
+            }).OrderByDescending(x => x.TeamMember).FirstOrDefault();
+            Console.WriteLine("{0}部 {1}人", samples.Name, samples.TeamMember);
         }
 
         private static void Exercise1_4(string file, string newfile) {
+            var xdoc = XDocument.Load(file);
+            var element = new XElement("ballsport",
+                new XElement("name","サッカー",new  XAttribute("kanji","蹴球")),
+                new XElement("teammembers","11"),
+                new XElement("firstplayed", "1863")
+                );
+            xdoc.Root.Add(element);
+            xdoc.Save(newfile);
            
         }
     }
---
+++ Chapter12/Section01/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+
+            var novels = new Novel[] {
+
+              new Novel {
+                Author = "ジェイムズ・P・ホーガン",
+                Title = "星を継ぐもの",
+                Published = 1977,
+              },
+              new Novel {
+                Author = "H・G・ウェルズ",
+                Title = "タイムマシン",
+                Published = 1895,
+              },
+
+            };
+
+            var novelCollection = new NovelCollection {
+                Novels = novels
+            };
+           
+
+
+            var settings = new XmlWriterSettings {
+                Encoding = new System.Text.UTF8Encoding(false),
+                Indent = true,
+                IndentChars = "  ",
+            };
+
+            //シリアル化リスト(リスト　１２－６)
+            /*using (var writer = XmlWriter.Create("novel.xml")) {
+                var serializer = new XmlSerializer(novelCollection.GetType());
+                serializer.Serialize(writer, novelCollection);
+            }*/
+
+            //逆シリアル化(リスト　12ー５)
+            using (var reader = XmlReader.Create("novel.xml")) {
+                //var serializer = new DataContractSerializer(typeof(Novel));
+                var serializer = new XmlSerializer(typeof(NovelCollection));
+                var novels1 = serializer.Deserialize(reader) as NovelCollection;
+                Console.WriteLine(novels1);
+
+                foreach (var novel in novels1.Novels) {
+                    Console.WriteLine(novel);
+                }
+            }
+        }
+    }
+}
