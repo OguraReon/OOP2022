107d6400f70bd205b5a4752c961755887442ee6b
伶音 小倉 <33127@ojs.ac.jp>
2022-07-12 14:14:00 +0900

    演習問題１２．２　終わり

--- Chapter12/Exercise02/Program.cs
+++ Chapter12/Exercise02/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
@@ -9,7 +11,8 @@ using System.Xml.Serialization;
 
 namespace Exercise02 {
     class Program {
-        static void Main(string[] args) {
+
+       public static void Main(string[] args) {
             var novelist = Exercise2_1("sample.xml");
 
             Exercise2_2(novelist, "novelist.json");
@@ -22,7 +25,7 @@ namespace Exercise02 {
             Console.WriteLine();
 
             // これは確認のためのコード 12.2.2
-           // Console.WriteLine(File.ReadAllText("novelist.json"));
+            Console.WriteLine(File.ReadAllText("novelist.json"));
             Console.WriteLine();
         }
 
@@ -35,14 +38,18 @@ namespace Exercise02 {
                 //Novelistクラスのオブジェクトを返却
                 return novels;
             }
-            
-
-            
-
         }
 
         private static void Exercise2_2(Novelist novelist, string file) {
-            
+            //シリアル化（JSON）
+            using (var stream = new FileStream(file, FileMode.Create, FileAccess.Write)) {
+                var serializer = new DataContractJsonSerializer(novelist.GetType(),
+                                 new DataContractJsonSerializerSettings {
+                                     DateTimeFormat = new DateTimeFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                                 });
+                serializer.WriteObject(stream, novelist);
+            }
+
         }
     }
 }
