64534d50bccee85c5d58ad97df3d9027f71299d4
伶音 小倉 <33127@ojs.ac.jp>
2022-09-26 15:53:39 +0900

    SampleEntityFramework　とちゅう

--- Chapter13/SampleEntityFramework/Program.cs
+++ Chapter13/SampleEntityFramework/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SampleEntityFramework.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,80 @@ using System.Threading.Tasks;
 namespace SampleEntityFramework {
     class Program {
         static void Main(string[] args) {
+            //InsertBooks();
+            //GetAllBooks();
+            foreach (var item in GetAllBooks()) {
+                Console.Write(item.PublishedYear + "年  ");
+                Console.WriteLine("著者" + item.Title);
+            }
+            //AddAuthors();
+
+        }
+
+        static void InsertBooks() {
+            using (var db = new BooksDbContext()) {
+                var book1 = new Book {
+                    Title = "坊ちゃん",
+                    PublishedYear = 2003,
+                    Author = new Author {
+                        Birthday = new DateTime(1867, 2, 9),
+                        Gender = "M",
+                        Name = "夏目漱石",
+                    }
+                };
+
+                db.Books.Add(book1);
+
+
+                var book2 = new Book {
+                    Title = "人間失格",
+                    PublishedYear = 1990,
+                    Author = new Author {
+                        Birthday = new DateTime(1909, 6, 19),
+                        Gender = "M",
+                        Name = "太宰治",
+                    }
+                };
+
+                db.Books.Add(book2);
+                db.SaveChanges();//データベースの更新
+
+
+
+            }
+        }
+
+        // List 13-9
+        private static void AddAuthors() {
+            using (var db = new BooksDbContext()) {
+                var author1 = new Author {
+                    Birthday = new DateTime(1878, 12, 7),
+                    Gender = "F",
+                    Name = "与謝野晶子"
+                };
+                db.Authors.Add(author1);
+                var author2 = new Author {
+                    Birthday = new DateTime(1896, 8, 27),
+                    Gender = "M",
+                    Name = "宮沢賢治"
+                };
+                db.Authors.Add(author2);
+                db.SaveChanges();
+            }
+        }
+
+        static IEnumerable<Book> GetAllBooks() {
+            using (var db = new BooksDbContext()) {
+
+                return db.Books.ToList();
+            }
+        }
+
+        static IEnumerable<Book> GetBooks() {
+            using (var db = new BooksDbContext()) {
+
+                return db.Books.Where(book => book.Author.Name.StartsWith("夏目"));
+            }
         }
     }
 }
