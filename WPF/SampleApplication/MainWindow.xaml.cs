7b4b3e263767c99a9147c10abc5c43e8bd17fb0c
伶音 小倉 <33127@ojs.ac.jp>
2022-10-17 14:10:53 +0900

    WPF 現在の季節をCBに自動入力

--- WPF/SampleApplication/MainWindow.xaml.cs
+++ WPF/SampleApplication/MainWindow.xaml.cs
@@ -20,29 +20,51 @@ namespace SampleApplication {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+          
+            seasonComboBox.SelectedIndex = GetSeasonIndex();
+            
         }
 
         private void checkBox_Checked(object sender, RoutedEventArgs e) {
-
+            checkBoxTextBlock.Text = "チェック済み";
         }
 
         private void checkBox_Unchecked(object sender, RoutedEventArgs e) {
-
+            checkBoxTextBlock.Text = "未チェック済み";
         }
 
         private void redRadioButton_Checked(object sender, RoutedEventArgs e) {
-
+            colorTextBox.Text = "赤";
         }
 
         private void yellowRadioButton_Checked(object sender, RoutedEventArgs e) {
-
+            colorTextBox.Text = "黄";
         }
 
         private void blueRadioButton_Checked(object sender, RoutedEventArgs e) {
-
+            colorTextBox.Text = "青";
         }
 
         private void seasonComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            seasonTextBlock.Text = (string)((ComboBoxItem)seasonComboBox.SelectedItem).Content;
+        }
+
+        private int GetSeasonIndex() {
+            
+            var nowMonth = DateTime.Now.Month;
+
+            if (nowMonth == 3 || nowMonth == 4 || nowMonth ==5) {
+                return 0;
+            }
+            if (nowMonth == 6 || nowMonth == 7 || nowMonth == 8) {
+                return 1;
+            }
+            if (nowMonth == 9 || nowMonth == 10 || nowMonth == 11) {
+                return 2;
+            }
+            return 3;
+
+
 
         }
     }
